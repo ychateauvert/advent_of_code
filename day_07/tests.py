22b557ea34cc19b23aa132b2cef84107907bca13
Yanick Châteauvert <ychateauvert@devalto.com>
2016-12-08 07:18:03 -0500

    Day 07

---
+++ day_07/tests.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import unittest
+from challenge import IpAddress
+
+def data_provider(data_provider):
+    def decorator(fn):
+        def repl(self, *args):
+            for i in data_provider:
+                fn(self, *i)
+        return repl
+    return decorator
+
+class TestChallege(unittest.TestCase):
+
+    provided_part_1 = [
+        (IpAddress('abba[mnop]qrst'), True),
+        (IpAddress('abcd[bddb]xyyx'), False),
+        (IpAddress('aaaa[qwer]tyui'), False),
+        (IpAddress('ioxxoj[asdfgh]zxcvbn'), True)
+    ]
+
+    provided_part_2 = [
+        (IpAddress('aba[bab]xyz'), True),
+        (IpAddress('xyx[xyx]xyx'), False),
+        (IpAddress('aaa[kek]eke'), True),
+        (IpAddress('zazbz[bzb]cdb'), True)
+    ]
+
+
+    # @data_provider(provided_part_1)
+    # def test_provided_part_1(self, ip_address, supportTls):
+        # self.assertEqual(supportTls, ip_address.is_supporting_tls())
+
+    @data_provider(provided_part_2)
+    def test_provided_part_2(self, ip_address, supportSsl):
+        self.assertEqual(supportSsl, ip_address.is_supporting_ssl())
+
+
+
+if __name__ == '__main__':
+    unittest.main()
