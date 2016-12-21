49d278deee81fe19d2af836b1d2c5a427eab7de4
Yanick Châteauvert <ychateauvert@devalto.com>
2016-12-20 21:37:42 -0500

    Day 20, part 2

--- day_20/day20.py
+++ day_20/day20.py
@@ -1,38 +1,24 @@
 #!/usr/bin/env python
 
-import sys
-import itertools
-import pdb
-
-blacklist_rules = itertools.chain.from_iterable([range(int(i[0]), int(i[1]) + 1) for i in [x.split("-") for x in open("input.txt").read().strip().split("\n")]])
-
-
-def is_blacklisted(value):
-    return value not in blacklist_rules
-
-
-def non_blacklisted(all_values):
-    """ Remove all blacklisted values
-
-    >>> remove_blacklisted(range(0, 10), [range(5, 9), range(0, 3), range(4, 8)])
-    {3, 9}
-    """
-    for i in all_values:
-        if i not in blacklist_rules:
-            print('Found %s' % i)
-            yield(i)
-
-
 def main():
-    global blacklist_rules
+    blacklist_intervals = [tuple(map(int, x.split("-"))) for x in open("input.txt").read().strip().split("\n")]
 
-    values = range(0, 4294967295)
+    valid_ips, current_ip, index = 0, 0, 0
+    blacklist_intervals.sort()
+    while current_ip < 4294967295:
+        low, high = blacklist_intervals[index]
+        if current_ip >= low:
+            if current_ip <= high:
+                # If we are within an interval skip to first ip after interval
+                current_ip = high + 1
+                continue
+            index += 1
+        else:
+            valid_ips += 1
+            current_ip += 1
 
-    # for valid in non_blacklisted(values):
-        # print('[Part 1] Found %s' % valid)
-        # sys.exit(0)
 
-    print("[Part 2] Valid ips: %s" % len(list(non_blacklisted(values))))
+    print("[Part 2] Valid ips %s" % valid_ips)
 
 
 if __name__ == '__main__':
