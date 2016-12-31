ca6bccb3aff05d9fd66b5369a948db808479f804
Yanick Châteauvert <ychateauvert@devalto.com>
2016-12-31 15:56:30 -0500

    Day 19 part 2

--- day_19.py
+++ day_19.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from itertools import cycle
+from math import floor
 
 CHALLENGE_INPUT = 3014603
 
@@ -19,13 +19,36 @@ def find_white_elephant_winner(participants_count):
         stole_from, stolen_gifts = gifts_distribution.pop(0)
         gifts_distribution.append((stealer, gifts + stolen_gifts))
 
-        print(len(gifts_distribution))
-
     return gifts_distribution.pop()[0]
 
 
+def find_white_elephant_across(participants_count):
+    """ Steal across
+
+    >>> find_white_elephant_across(5)
+    2
+    """
+    participants = list(range(1, participants_count+1))
+    gifts_distribution = {i: 1 for i in participants}
+
+    while len(participants) > 1:
+        stole_from_idx = floor(len(participants)/2)
+        # print(participants)
+        # print(gifts_distribution)
+        stole_from = participants.pop(stole_from_idx)
+        stealer = participants.pop(0)
+
+        gifts_distribution[stealer] += gifts_distribution[stole_from]
+        gifts_distribution[stole_from] = 0
+        participants.append(stealer)
+        # print("Stealer: %s, Stolen from %s" % (stealer, stole_from))
+
+    return participants[0]
+
+
 def main():
-    print("[Part 1] The winner is %s" % find_white_elephant_winner(CHALLENGE_INPUT))
+    # print("[Part 1] The winner is %s" % find_white_elephant_winner(CHALLENGE_INPUT))
+    print("[Part 2] The winner is %s" % find_white_elephant_across(CHALLENGE_INPUT))
 
 
 if __name__ == '__main__':
