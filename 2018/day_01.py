ee8b8151e5808ad94f780e1befd0eaf66738ab72
Yanick Châteauvert <ychateauvert@devalto.com>
2018-12-01 07:46:59 -0500

    Day 01

---
+++ 2018/day_01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from itertools import cycle
+
+value = 0
+
+reached = set()
+
+frequencies = open('inputs/day_01.txt').read().strip().splitlines()
+for frequency in frequencies:
+    value = value + int(frequency)
+
+print('Day 01: %s' % value)
+
+value = 0
+for frequency in cycle(frequencies):
+    value = value + int(frequency)
+    if value in reached:
+        print('Day 02: %s' % value)
+        exit()
+    reached.add(value)
