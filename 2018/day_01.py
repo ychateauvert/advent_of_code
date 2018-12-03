c11f37b2ea4f5e89caafbd25d31484fe49a09211
Yanick Châteauvert <ychateauvert@devalto.com>
2018-12-03 15:03:17 -0500

    Fix user facing string

--- 2018/day_01.py
+++ 2018/day_01.py
@@ -10,12 +10,12 @@ frequencies = open('inputs/day_01.txt').read().strip().splitlines()
 for frequency in frequencies:
     value = value + int(frequency)
 
-print('Day 01: %s' % value)
+print('Day 01 part 1: %s' % value)
 
 value = 0
 for frequency in cycle(frequencies):
     value = value + int(frequency)
     if value in reached:
-        print('Day 02: %s' % value)
+        print('Day 01 part 2: %s' % value)
         exit()
     reached.add(value)
