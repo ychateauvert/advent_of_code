6f0d077db450d55a83cefb6d8838f2c1b725a74b
Yanick Châteauvert <ychateauvert@devalto.com>
2019-12-02 20:13:07 -0500

    2019-02 Refactor into methods

--- 2019/day_02.py
+++ 2019/day_02.py
@@ -1,11 +1,43 @@
 #!/usr/bin/env python
 
-import sys
+raw_puzzle = open('inputs/day_02.txt').read().strip()
+puzzle_input = [int(x) for x in raw_puzzle.split(',')]
 
-original_memory = [int(x) for x in open('inputs/day_02.txt').read().strip().split(',')]
+
+def s_to_m(memory_str):
+    return [int(x) for x in memory_str.split(',')]
 
 
-def run_instruction(memory, position):
+def m_to_s(memory):
+    return ",".join([str(x) for x in memory])
+
+
+def add(memory, address):
+    _, x, y, output = memory[address:address+4]
+    memory[output] = memory[x] + memory[y]
+
+    return address + 4
+
+
+def multiply(memory, address):
+    _, x, y, output = memory[address:address+4]
+    memory[output] = memory[x] * memory[y]
+
+    return address + 4
+
+
+def abort(memory, address):
+    return False
+
+
+opcodes = {
+    1: add,
+    2: multiply,
+    99: abort
+}
+
+
+def run_instruction(memory, position=0):
     if memory[position] == 99:
         return False
     opcode, first_val, second_val, output = memory[position:position+4]
@@ -18,37 +50,59 @@ def run_instruction(memory, position):
 
 
 def run_program(memory, position=0):
-    while run_instruction(memory, position):
+    """
+    >>> current_execution(s_to_m('1,0,0,0,99'))[0]
+    2
+    >>> current_execution(s_to_m('2,3,0,3,99'))[0]
+    2
+    >>> current_execution(s_to_m('2,4,4,5,99,0'))[0]
+    2
+    >>> current_execution(s_to_m('1,1,1,4,99,5,6,0,99'))[0]
+    30
+    """
+    current_execution = memory.copy()
+    while run_instruction(current_execution, position):
         position += 4
 
+    return current_execution
 
-def part1_run_program(opcodes):
+
+def part1(memory):
+    memory = memory.copy()
+    memory[1], memory[2] = 12, 2
+
+    return run_program(memory)[0]
+
+
+def part2(memory):
+    for i in range(100):
+        for j in range(100):
+            part2_memory = memory.copy()
+            part2_memory[1], part2_memory[2] = i, j
+
+            if run_program(part2_memory)[0] == 19690720:
+                return 100 * i + j
+
+
+def main():
+    # Result: 2692315
+    print(f"Part 1: {part1(puzzle_input)}")
+    print(f"Part 2: {part2(puzzle_input)}")
+
+
+if __name__ == '__main__':
+    main()
+
+
+def test_part1(puzzle):
     """
-    >>> part1_run_program('1,0,0,0,99')
+    >>> test_part1('1,0,0,0,99')
     '2,0,0,0,99'
-    >>> part1_run_program('2,3,0,3,99')
+    >>> test_part1('2,3,0,3,99')
     '2,3,0,6,99'
-    >>> part1_run_program('2,4,4,5,99,0')
+    >>> test_part1('2,4,4,5,99,0')
     '2,4,4,5,99,9801'
     """
-    program = [int(x) for x in opcodes.split(',')]
+    program = [int(x) for x in puzzle.split(',')]
     run_program(program)
     return ",".join([str(x) for x in program])
-
-
-part1_memory = original_memory.copy()
-part1_memory[1], part1_memory[2] = 12, 2
-run_program(part1_memory)
-print(f"Part 1: {part1_memory[0]}")
-# Result: 2692315
-
-for i in range(100):
-    for j in range(100):
-        part2_memory = original_memory.copy()
-        part2_memory[1], part2_memory[2] = i, j
-
-        run_program(part2_memory)
-
-        if part2_memory[0] == 19690720:
-            print(f"Part 2: {100 * i + j}")
-            sys.exit()
